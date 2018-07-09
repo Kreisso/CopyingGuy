cbab996f6603a416311145f66aa143ac59894306
Maciej Polak <polakmaciej.web@gmail.com>
2018-07-10 00:02:55 +0200

    Adding dynamic recognition of files extension

--- Operations.java
+++ Operations.java
@@ -3,6 +3,7 @@ package cg;
 /**
  * Created by kreisso on 09.07.2018.
  */
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -10,14 +11,23 @@ import java.nio.file.StandardCopyOption;
 
 public interface Operations {
 
+
+
     default public void makeCopies(String source, String destiny, Integer howManyCopies) throws IOException
     {
         if(!Files.exists(Paths.get(destiny)))
         {
+
             Files.createDirectory(Paths.get(destiny));
         }
+
+        String fileName = new File(source).getName();
+        int dotIndex = fileName.lastIndexOf('.');
+        String extension = (dotIndex == -1) ? "" : fileName.substring(dotIndex + 1);
+
+
         for(int i = 0; i < howManyCopies.intValue(); i++) {
-            Files.copy(Paths.get(source), Paths.get(destiny+"/"+i+".png"), StandardCopyOption.REPLACE_EXISTING);
+            Files.copy(Paths.get(source), Paths.get(destiny+"/"+i+"."+extension), StandardCopyOption.REPLACE_EXISTING);
         }
     }
 }
